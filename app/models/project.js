b36e42c2c3f9d6d362bed3780fa5e8079ec19e2f
Ryan LaBouve <imryanlabouve@gmail.com>
2014-12-14 00:19:13 -0600

    Add project model route and controller

---
+++ app/models/project.js
@@ -0,0 +1,33 @@
+import DS from 'ember-data';
+
+var Project = DS.Model.extend({
+  name: DS.attr('string'),
+  thumbnail: DS.attr('string'),
+  cover: DS.attr('string'),
+  type: DS.attr('string'),
+  description: DS.attr('string'),
+  // items: DS.belongsTo('item')
+});
+
+Project.reopenClass({
+  FIXTURES: [
+    {
+        id: 1,
+        name: "Project 1",
+        thumbnail: "http://placehold.it/300x300",
+        cover: "http://placehold.it/600x600",
+        type: "photo",
+        description: "Vero amet modi dicta autem aut ut. Occaecati iste deleniti dolores deleniti dolorem quia. Ut eos quas fuga ex libero et. Unde aspernatur maiores excepturi eum vero."
+    },
+    {
+        id: 2,
+        name: "Project 2",
+        thumbnail: "http://placehold.it/300x300",
+        cover: "http://placehold.it/600x600",
+        type: "photo",
+        description: "Amet enim minus eos ipsa veniam cum perspiciatis accusamus. Ab dicta dolor est. Blanditiis nostrum fugiat iure dolores qui ad. Et sit maiores at dignissimos."
+    },
+]
+});
+
+export default Project;
