4d18c0d82d58aa9b156d819c5f9acebadec6b5fd
Ryan LaBouve <imryanlabouve@gmail.com>
2014-12-13 23:54:41 -0600

    Stubb out top and bottom

--- config/environment.js
+++ config/environment.js
@@ -26,6 +26,16 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.contentSecurityPolicy = {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-eval' localhost:35799 0.0.0.0:35799",
+      'font-src': "'self' http://fonts.gstatic.com http://maxcdn.bootstrapcdn.com",
+      'connect-src': "'self' ws://localhost:35799 ws://0.0.0.0:35799",
+      'img-src': "'self'",
+      'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com http://maxcdn.bootstrapcdn.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
+      'media-src': "'self'"
+    }
   }
 
   if (environment === 'test') {
