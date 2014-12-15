cbbac22a9beb0483b1b45a4b0df124433fff6e0d
Ryan LaBouve <imryanlabouve@gmail.com>
2014-12-15 12:40:16 -0600

    Stub out nav and mobile

--- app/models/project.js
+++ app/models/project.js
@@ -15,7 +15,7 @@ Project.reopenClass({
         id: 1,
         name: "Project 1",
         thumbnail: "http://placehold.it/400x300",
-        cover: "http://placehold.it/1200x400",
+        cover: "http://placehold.it/1200x600",
         type: "photo",
         description: "Vero amet modi dicta autem aut ut. Occaecati iste deleniti dolores deleniti dolorem quia. Ut eos quas fuga ex libero et. Unde aspernatur maiores excepturi eum vero."
     },
@@ -23,7 +23,39 @@ Project.reopenClass({
         id: 2,
         name: "Project 2",
         thumbnail: "http://placehold.it/400x300",
-        cover: "http://placehold.it/1200x400",
+        cover: "http://placehold.it/1200x600",
+        type: "photo",
+        description: "Amet enim minus eos ipsa veniam cum perspiciatis accusamus. Ab dicta dolor est. Blanditiis nostrum fugiat iure dolores qui ad. Et sit maiores at dignissimos."
+    },
+    {
+        id: 3,
+        name: "Project 3",
+        thumbnail: "http://placehold.it/400x300",
+        cover: "http://placehold.it/1200x600",
+        type: "photo",
+        description: "Amet enim minus eos ipsa veniam cum perspiciatis accusamus. Ab dicta dolor est. Blanditiis nostrum fugiat iure dolores qui ad. Et sit maiores at dignissimos."
+    },
+    {
+        id: 4,
+        name: "Project 4",
+        thumbnail: "http://placehold.it/400x300",
+        cover: "http://placehold.it/1200x600",
+        type: "photo",
+        description: "Amet enim minus eos ipsa veniam cum perspiciatis accusamus. Ab dicta dolor est. Blanditiis nostrum fugiat iure dolores qui ad. Et sit maiores at dignissimos."
+    },
+    {
+        id: 5,
+        name: "Project 5",
+        thumbnail: "http://placehold.it/400x300",
+        cover: "http://placehold.it/1200x600",
+        type: "photo",
+        description: "Amet enim minus eos ipsa veniam cum perspiciatis accusamus. Ab dicta dolor est. Blanditiis nostrum fugiat iure dolores qui ad. Et sit maiores at dignissimos."
+    },
+    {
+        id: 6,
+        name: "Project 6",
+        thumbnail: "http://placehold.it/400x300",
+        cover: "http://placehold.it/1200x600",
         type: "photo",
         description: "Amet enim minus eos ipsa veniam cum perspiciatis accusamus. Ab dicta dolor est. Blanditiis nostrum fugiat iure dolores qui ad. Et sit maiores at dignissimos."
     },
