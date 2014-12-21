53ceb6c6087e9984885f3bb28696c572fd2581fa
Ryan LaBouve <imryanlabouve@gmail.com>
2014-12-20 23:30:24 -0600

    Add a few sets of photos

--- app/models/item.js
+++ app/models/item.js
@@ -19,10 +19,94 @@ Item.reopenClass({
     },
     {
         id: 2,
-        name: "Item 2",
-        cover: "http://placehold.it/1200x600",
-        description: "bobs",
-        project: 1
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/Loveworks1-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 3,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks2-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 4,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks3-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 5,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks4-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 6,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks5-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 7,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks6-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 8,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks7-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 9,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks8-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 10,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks9-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 11,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks10-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 12,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks11-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 13,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks12-item.jpg",
+        description: "",
+        project: 2
+    },
+    {
+        id: 14,
+        name: "",
+        cover: "img/event-loveworks-leadership-inc/loveworks13-item.jpg",
+        description: "",
+        project: 2
     },
   ]
 });
